25e43155ef15c41413cac7b0bb13730c1a7e6f44
AdesholaDaniel <dannyjake0001@gmail.com>
2022-04-25 12:59:46 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import { ChatEngine } from 'react-chat-engine';
-import ChatFeed from './components/ChatFeed';
 
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = 'ee1063a1-09df-4e57-8120-9084c3848ca8';
+
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="ee1063a1-09df-4e57-8120-9084c3848ca8"
-      userName="adeshdaniel"
-      userSecret="123456"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          'https://chat-engine-assets.s3.amazonaws.com/click.mp3'
+        ).play()
+      }
     />
   );
 };
+
+// infinite scroll, logout, more customizations...
 
 export default App;
